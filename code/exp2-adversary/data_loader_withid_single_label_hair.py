3d0003741dab6692dc9ba0173a9f1b2dad346ff8
Fatemeh Mireshghallah <fatemehmireshghallah@Fatemehs-MacBook-Pro.local>
2021-02-14 19:27:31 -0800

    code

---
+++ code/exp2-adversary/data_loader_withid_single_label_hair.py
@@ -0,0 +1,137 @@
+from torch.utils import data
+from torchvision import transforms as T
+from torchvision.datasets import ImageFolder
+from PIL import Image
+import torch
+import os
+import random
+
+
+class CelebA(data.Dataset):
+    """Dataset class for the CelebA dataset."""
+
+    def __init__(self, image_dir, attr_path, id_path, selected_attrs, transform, mode):
+        """Initialize and preprocess the CelebA dataset."""
+        self.image_dir = image_dir
+        self.attr_path = attr_path
+        self.id_path = id_path
+        self.selected_attrs = selected_attrs
+        self.transform = transform
+        self.mode = mode
+        self.train_dataset = []
+        self.test_dataset = []
+        self.attr2idx = {}
+        self.idx2attr = {}
+        self.preprocess()
+
+        if mode == 'train':
+            self.num_images = len(self.train_dataset)
+        else:
+            self.num_images = len(self.test_dataset)
+
+    def preprocess(self):
+        """Preprocess the CelebA attribute file."""
+        lines = [line.rstrip() for line in open(self.attr_path, 'r')]
+        lines_id = [line.rstrip() for line in open(self.id_path, 'r')]
+        all_attr_names = lines[1].split() #This is 1 because line 0 is the len 
+        all_attr_names.append('id')
+        count_hair = 0
+        for i, attr_name in enumerate(all_attr_names):
+            self.attr2idx[attr_name] = i
+            self.idx2attr[i] = attr_name
+
+        lines = lines[2:]
+        #produces same results each time
+        random.seed(5534)
+        random.shuffle(lines)
+        for i, line in enumerate(lines):
+            flag = 0
+            split_id = lines_id[i].split()
+            split = line.split()
+            
+            filename = split[0]
+            values = split[1:]
+            id = split_id[1]
+            values.append(id)
+            
+            label = []
+            #label = 0
+            count = 0
+            for j, attr_name in enumerate(self.selected_attrs):
+                idx = self.attr2idx[attr_name]
+                #for the id, we do separate
+                if (attr_name == 'id'):
+                    label.append(int(values[idx]))
+                else:
+                    label.append(values[idx] == '1')
+                    if (not values[idx] == '1'):
+                        count_hair += 1
+                        flag= 1
+                #count += (int(values[idx] == '1')* (2**j))
+            #label.append(count)
+            if (not flag) or (flag and count_hair<48472):
+                if i%10 ==0 and len(self.test_dataset) < 10000 :
+                    self.test_dataset.append([filename, label])
+                else:
+                    self.train_dataset.append([filename, label])
+
+        print('Finished preprocessing the CelebA dataset...')
+
+    def __getitem__(self, index):
+        """Return one image and its corresponding attribute label."""
+        dataset = self.train_dataset if self.mode == 'train' else self.test_dataset
+        filename, label = dataset[index]
+        #print(filename, label)
+        image = Image.open(os.path.join(self.image_dir, filename))
+        return self.transform(image), torch.FloatTensor(label)
+
+    def __len__(self):
+        """Return the number of images."""
+        return self.num_images
+
+
+def get_loader(image_dir, attr_path, id_path, selected_attrs, crop_size=178, image_size=128, 
+               batch_size=16, dataset='CelebA', mode='train', num_workers=1):
+    """Build and return a data loader."""
+    transform = []
+    if mode == 'train':
+        transform.append(T.RandomHorizontalFlip())
+    transform.append(T.CenterCrop(crop_size))
+    transform.append(T.Resize(image_size))
+    transform.append(T.ToTensor())
+    transform.append(T.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5)))
+    transform = T.Compose(transform)
+
+    if dataset == 'CelebA':
+        dataset = CelebA(image_dir, attr_path, id_path, selected_attrs, transform, mode)
+    elif dataset == 'RaFD':
+        dataset = ImageFolder(image_dir, transform)
+
+    data_loader = data.DataLoader(dataset=dataset,
+                                  batch_size=batch_size,
+                                  shuffle= (mode=='train'),
+                                  num_workers=num_workers) #(mode=='train')
+    return data_loader, dataset
+
+def get_dataset(image_dir, attr_path, id_path, selected_attrs, crop_size=178, image_size=128, 
+               batch_size=16, dataset='CelebA', mode='train', num_workers=1):
+    
+    transform = []
+    if mode == 'train':
+        transform.append(T.RandomHorizontalFlip())
+    transform.append(T.CenterCrop(crop_size))
+    transform.append(T.Resize(image_size))
+    transform.append(T.ToTensor())
+    transform.append(T.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5)))
+    transform = T.Compose(transform)
+
+    if dataset == 'CelebA':
+        dataset = CelebA(image_dir, attr_path, id_path,selected_attrs, transform, mode)
+    elif dataset == 'RaFD':
+        dataset = ImageFolder(image_dir, transform)
+
+    data_loader = data.DataLoader(dataset=dataset,
+                                  batch_size=batch_size,
+                                  shuffle=(mode=='train'),
+                                  num_workers=num_workers)
+    return dataset
---
+++ code/exp3-black-box/vgg16_2.py
@@ -0,0 +1,117 @@
+import cv2
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchfile
+from collections import OrderedDict
+
+class VGG_16_2(nn.Module):
+    """
+    Main Class
+    """
+
+    def __init__(self):
+        """
+        Constructor
+        """
+        super(VGG_16_2, self).__init__()
+        self.convnet = nn.Sequential(OrderedDict([
+            ('conv_1_1', nn.Conv2d(3, 64, 3, stride=1, padding=1) ),
+            ('relu1', nn.ReLU()),
+            ('conv_1_2', nn.Conv2d(64, 64, 3, stride=1, padding=1) ),
+            ('relu2', nn.ReLU()),
+            ('max1', nn.MaxPool2d(kernel_size=(2, 2))),
+            ('conv_2_1',  nn.Conv2d(64, 128, 3, stride=1, padding=1) ),
+            ('relu3', nn.ReLU()),
+            ('conv_2_2',  nn.Conv2d(128, 128, 3, stride=1, padding=1) ),
+            ('relu4', nn.ReLU()),
+            ('max2', nn.MaxPool2d(kernel_size=(2, 2))), 
+            ('conv_3_1', nn.Conv2d(128, 256, 3, stride=1, padding=1) ),
+            ('relu5', nn.ReLU()),
+            ('conv_3_2', nn.Conv2d(256, 256, 3, stride=1, padding=1) ),
+            ('relu6', nn.ReLU()),
+            ('conv_3_3', nn.Conv2d(256, 256, 3, stride=1, padding=1) ),
+            ('relu7', nn.ReLU()),
+            ('max3', nn.MaxPool2d(kernel_size=(2, 2))),
+            ('conv_4_1',  nn.Conv2d(256, 512, 3, stride=1, padding=1) ),
+            ('relu13', nn.ReLU()),
+            ('conv_4_2',nn.Conv2d(512, 512, 3, stride=1, padding=1)),
+            ('relu8', nn.ReLU()),
+            ('conv_4_3', nn.Conv2d(512, 512, 3, stride=1, padding=1)),
+            ('relu9', nn.ReLU()),
+            ('max4', nn.MaxPool2d(kernel_size=(2, 2))),
+            ('conv_5_1',  nn.Conv2d(512, 512, 3, stride=1, padding=1) ),
+            ('relu10', nn.ReLU()),
+            ('conv_5_2',nn.Conv2d(512, 512, 3, stride=1, padding=1)),
+            ('relu11', nn.ReLU()),
+            ('conv_5_3', nn.Conv2d(512, 512, 3, stride=1, padding=1)),
+            ('relu12', nn.ReLU()),
+            ('max5', nn.MaxPool2d(kernel_size=(2, 2)))
+
+               
+               
+        ]))
+
+        self.fc = nn.Sequential(OrderedDict([
+            ('f6', nn.Linear(512 * 7 * 7, 4096)),
+            ('relu6', nn.ReLU()),
+            ('f7', nn.Linear(4096, 4096)),
+            ('relu7', nn.ReLU()),
+            ('f8', nn.Linear(4096,2))
+            
+        ]))
+
+
+        
+        self.block_size = [2, 2, 3, 3, 3]
+     
+       
+
+
+    def load_weights(self, path="pretrained/vgg_face_torch/VGG_FACE.t7"):
+        """ Function to load luatorch pretrained
+
+        Args:
+            path: path for the luatorch pretrained
+        """
+        model = torchfile.load(path)
+        counter = 1
+        block = 1
+        for i, layer in enumerate(model.modules):
+            if layer.weight is not None:
+                if block <= 5:
+                    self_layer = getattr(self, "conv_%d_%d" % (block, counter))
+                    counter += 1
+                    if counter > self.block_size[block - 1]:
+                        counter = 1
+                        block += 1
+                    self_layer.weight.data[...] = torch.tensor(layer.weight).view_as(self_layer.weight)[...]
+                    self_layer.bias.data[...] = torch.tensor(layer.bias).view_as(self_layer.bias)[...]
+                else:
+                    self_layer = getattr(self, "fc%d" % (block))
+                    block += 1
+                    self_layer.weight.data[...] = torch.tensor(layer.weight).view_as(self_layer.weight)[...]
+                    self_layer.bias.data[...] = torch.tensor(layer.bias).view_as(self_layer.bias)[...]
+
+    def forward(self, x):
+        """ Pytorch forward
+
+        Args:
+            x: input image (224x224)
+
+        Returns: class logits
+
+        """
+
+        
+
+       
+     
+ 
+     
+        x = self.convnet(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        #x = F.softmax(self.fc8(x), dim=1)
+        return x
+
